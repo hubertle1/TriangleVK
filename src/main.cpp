a70410d9ffc5d5e83e40e0808253ab4807e461b9
Hubert Lewandowski <hubert1.lewandowski@intel.com>
2024-07-25 13:19:47 +0200

    Add basic vk instancing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 ﻿#pragma once
 #include <iostream>
-
-using namespace std;
+#include <vulkan/vulkan.h>
 
 int main()
 {
-	cout << "Hello World" << endl;
+	VkApplicationInfo appInfo =
+	{
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pApplicationName = "Triangle Vulkan App"
+	};
+
+	VkInstanceCreateInfo instanceCreateInfo =
+	{
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &appInfo
+	};
+
+	VkInstance instance;
+
+	auto result = vkCreateInstance( &instanceCreateInfo, nullptr, &instance );
+	if( result == VK_SUCCESS )
+	{
+		std::cout << "Vulkan instance initialized successfully!" << std::endl;
+	}
+
 	return 0;
 }
