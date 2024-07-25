75f32f7d6c95efd1ba0b782882626926331a1188
Hubert Lewandowski <hubert1.lewandowski@intel.com>
2024-07-25 11:47:45 +0200

    Add project files.

---
+++ TriangleVK.cpp
@@ -0,0 +1,12 @@
+﻿// TriangleVK.cpp : Defines the entry point for the application.
+//
+
+#include "TriangleVK.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
