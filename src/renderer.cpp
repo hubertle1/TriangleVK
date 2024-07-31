c82ebd2e6bddfa536ff8bdf807f96eda18aea9fb
Hubert Lewandowski <hubert1.lewandowski@intel.com>
2024-07-31 09:31:02 +0200

    Added: * GPU physical and logical device setup * Verbose logging * Refactoring

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -3,13 +3,14 @@
 #include <array>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 #include <vulkan/vulkan_win32.h>
 
 Renderer::Renderer( const Window& window )
 {
 	this->SetupInstance();
 	this->SetupSurface( window );
-	
+	this->SetupGPU();
 }
 
 void Renderer::SetupInstance()
@@ -35,8 +36,10 @@ void Renderer::SetupInstance()
 		.ppEnabledExtensionNames = extensions.data(),
 	};
 
-	auto result = vkCreateInstance( &instanceCreateInfo, nullptr, &this->context.instance );
-	this->Validate( result );
+	this->Validate(
+		vkCreateInstance( &instanceCreateInfo, nullptr, &this->context.instance ), 
+		"Create instance"
+	);
 }
 
 void Renderer::SetupSurface( const Window& window )
@@ -48,14 +51,79 @@ void Renderer::SetupSurface( const Window& window )
 		.hwnd = window.GetWindow()
 	};
 
-	this->Validate( vkCreateWin32SurfaceKHR( this->context.instance, &surfaceInfo, 0, &this->context.surface ) );
+	this->Validate(
+		vkCreateWin32SurfaceKHR( this->context.instance, &surfaceInfo, 0, &this->context.surface ),
+		"Create surface"
+	);
 }
 
-void Renderer::Validate( VkResult result )
+void Renderer::SetupGPU()
+{
+	uint32_t gpusAvailable = 0;
+	this->Validate(
+		vkEnumeratePhysicalDevices( this->context.instance, &gpusAvailable, 0 ),
+		"Get physical devices count"
+	);
+
+	std::vector<VkPhysicalDevice> gpus(gpusAvailable);
+	this->Validate(
+		vkEnumeratePhysicalDevices( this->context.instance, &gpusAvailable, gpus.data()),
+		"Get physical devices data"
+	);
+
+	for( const auto& gpu : gpus )
+	{
+		auto index = this->GetGPUIndex( gpu );
+
+		this->context.gpu.physicalDevice = gpu;
+		this->context.gpu.index = index;
+	}
+
+	VkDeviceCreateInfo deviceInfo =
+	{
+		.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO
+	};
+
+	this->Validate( 
+		vkCreateDevice( this->context.gpu.physicalDevice, &deviceInfo, 0, &this->context.gpu.logicalDevice ),
+		"Detect GPU"
+	);
+}
+
+uint32_t Renderer::GetGPUIndex( const VkPhysicalDevice& gpu ) const
+{
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties( gpu, &queueFamilyCount, 0 );
+
+	std::vector<VkQueueFamilyProperties> queueProps( queueFamilyCount );
+	vkGetPhysicalDeviceQueueFamilyProperties( gpu, &queueFamilyCount, queueProps.data() );
+
+	for( uint32_t i = 0; i < queueProps.size(); ++i )
+	{
+		VkBool32 doesSupportSurface = VK_FALSE;
+		this->Validate(
+			vkGetPhysicalDeviceSurfaceSupportKHR( gpu, i, this->context.surface, &doesSupportSurface ),
+			"Check for GPU surface support"
+		);
+
+		if( queueProps[ i ].queueFlags & VK_QUEUE_GRAPHICS_BIT && doesSupportSurface )
+		{
+			return i;
+		}
+	}
+
+	throw std::runtime_error( "GPU was not detected!" );
+}
+
+void Renderer::Validate( VkResult result, const std::string& whatWasValidatedMessage ) const
 {
 	if( result != VK_SUCCESS )
 	{
 		std::cerr << "Vulkan returned status: " << result << std::endl;
 		__debugbreak();
+	}
+	else
+	{
+		std::cout << "The task: '" << whatWasValidatedMessage << "' was successful." << std::endl;
 	}
 }
--- src/renderer.h
+++ src/renderer.h
@@ -12,9 +12,19 @@ private:
 	{
 		VkInstance instance;
 		VkSurfaceKHR surface;
+
+		struct GPU
+		{
+			VkPhysicalDevice physicalDevice;
+			VkDevice logicalDevice;
+			uint32_t index;
+		} gpu;
 	} context;
 
 	void SetupInstance();
 	void SetupSurface( const Window& window );
-	void Validate( VkResult result );
+	void SetupGPU();
+	uint32_t GetGPUIndex( const VkPhysicalDevice& gpu ) const;
+
+	void Validate( VkResult result, const std::string& whatWasValidatedMessage ) const;
 };
