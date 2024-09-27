c5c9e28127e65c4b29185a077523bb153caedf17
Hubert Lewandowski <hubert1.lewandowski@intel.com>
2024-09-27 11:33:01 +0200

    Remove GLM dependency

--- src/core/context.h
+++ src/core/context.h
@@ -6,38 +6,11 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <array>
-#include <glm/vec2.hpp>
 
 struct Vertex
 {
-	glm::vec2 position;
-	glm::vec2 texCoord;
-	
-    static VkVertexInputBindingDescription GetBindingDescription()
-    {
-        VkVertexInputBindingDescription bindingDescription = {};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Vertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        return bindingDescription;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 2> GetAttributeDescriptions()
-    {
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
-
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, position);
-
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, texCoord);
-
-        return attributeDescriptions;
-    }
+	float position[ 2 ] = {};
+	float texCoord[ 2 ] = {};
 };
 
 struct VulkanContext
